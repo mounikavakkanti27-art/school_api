0e9158bb48821e277a6bf155f9342fc62ba8e8f2
Mounika Vakkanti <mounikavakkanti27@gmail.com>
2026-04-28 22:11:26 -0400

    "template redirect, render, and url_for"

--- templates.py
+++ templates.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,5 +10,13 @@ def home():
 def about():
     return render_template('index.html', content = "interviews", loop= ["python", "Flask", "HTML","CSS","DSA", "DE", "ML", "AI", "DS", "SQL", "NoSQL"])
 
+@app.route('/sub_selection')
+def sub_selection():
+    return redirect(url_for('about'))
+
 if __name__ == '__main__':
   app.run(debug=True, port=5001)
+
+ #in this covered render_template - helps to navigate html template
+ # redirect - helps to redirect method
+ # url_for - helps for the url_for the method. 
