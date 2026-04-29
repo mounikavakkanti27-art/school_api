bdf8ef65fef8fa519b4568229aa30f2d901d9fb9
Mounika Vakkanti <mounikavakkanti27@gmail.com>
2026-04-29 13:54:51 -0400

    "render template and redirect for the template inheritence."

--- templates.py
+++ templates.py
@@ -2,21 +2,19 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #http://127.0.0.1:5001/
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('index.html', content = "interviews", loop= ["python", "Flask", "HTML","CSS","DSA", "DE", "ML", "AI", "DS", "SQL", "NoSQL"])
-
-@app.route('/sub_selection')
-def sub_selection():
-    return redirect(url_for('about'))
+@app.route("/courses") #http://127.0.0.1:5001/courses
+def courses():
+    return render_template('about.html', course_list=['Python', 'Flask','DSA','ML', 'AI', 'Data Science'], context='I am learning below courses.')
 
 if __name__ == '__main__':
-  app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001)
 
  #in this covered render_template - helps to navigate html template
  # redirect - helps to redirect method
  # url_for - helps for the url_for the method. 
+
+ #render_template - has extention to render the inheritance of base.html url.
